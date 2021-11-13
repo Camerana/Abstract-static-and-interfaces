cf802b9425137a2bcad498eeef4953bb82b40c99
turtleseverywhere <s.samele@libero.it>
2021-11-13 17:04:36 +0100

    Add project files.

---
+++ Abstract, static and interfaces/Classes.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract__static_and_interfaces
+{
+    /* 
+     * le interfacce non ammettono modificatori di visibilità: public, private etc..
+     * perché al loro interno tutto è public di default
+     * le interfacce non ammettono le parole chiave static e virtual
+     * possono avere delle properties, ma non dei fields
+     */
+    public interface IClass
+    {
+        string nome { get; set; }
+        string cognome;
+        public static void staticfunc();
+        public void func();
+        virtual void func();
+    }
+    /*
+     * una classe può implementare una o più interfacce...
+     */
+    public class Class0 : IClass
+    {
+        public string nome { get; set; }
+        public static void staticfunc() { }
+        public void func() { }
+    }
+    /*
+     * ...e ne deve possedere tutti i membri
+     */
+    public class Class1 : IClass
+    {
+        //public string nome { get; set; }
+        public static void staticfunc() { }
+        public void func() { }
+    }
+    /*
+     * una classe astratta può ereditare da altre classi (non statiche)
+     * e implementare interfacce*/
+    public abstract class Class2 : Class1, IClass
+    {
+        public string nome { get; set; }
+
+        /*
+         * da un contesto statico (in questo caso dall'interno di staticfunc) 
+         * non si può richiamare base, visto che non c'è l'instanza corrente,
+         * ma ci si può accedere tramite il nome della classe (come da norma).
+         * una funzione statica inoltre non può essere virtual
+         */
+        public static virtual void staticfunc()
+        {
+            base.staticfunc();
+            Class1.staticfunc();
+        }
+        /*
+         * l'unico modificatore possibile di un membro ereditato
+         * da un'interfaccia è public
+         */
+        //private void func() { }
+        //protected void func() { }
+        public virtual void func()
+        {
+            //base.staticfunc();
+            base.func();
+        }
+    }
+    /*
+     * una classe statica non può ereditare da classi
+     * e non può implementare interfacce*/
+    public static class Class3 : Class1, IClass
+    {
+        /*
+         * le funzioni possono avere i soliti modificatori,
+         * ma non fields non statici e funzioni non statiche
+         */
+        public static string cognome;
+        public string nome;
+
+        //private static void staticfunc() { }
+        //protected static void staticfunc() { }
+        public static void staticfunc() { }
+
+        //private void func() { }
+        //protected void func() { }
+        public void func() { }
+    }
+
+    /*
+     * una classe può ereditare da una classe e implementare una o più interfacce
+     */
+    public class Class4 : Class1, IClass
+    {
+        public static void staticfunc() { }
+        public void func() { }
+    }
+
+    /*
+     * una classe può ereditare da una classe astratta e implementare una o più interfacce
+     */
+    public class Class5 : Class2, IClass
+    {
+        public static override void staticfunc()
+        {
+            base.staticfunc();
+        }
+        public override void func()
+        {
+            base.func();
+        }
+    }
+
+    /*
+     * una classe non può ereditare da una classe statica
+     */
+    public class Class6 : Class3
+    {
+    }
+
+    /*
+     * una classe statica non può ereditare da una classe statica
+     */
+    public static class Class7 : Class3
+    {
+    }
+}
---
+++ Abstract, static and interfaces/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract__static_and_interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //un'interfaccia non è istanziabile
+            IClass ic = new IClass();
+            ic.func();
+
+            Class0 c0 = new Class0();
+            c0.nome = "c0";
+            c0.func();
+            c0.staticfunc();
+            Class0.staticfunc();
+
+            Class0 c01 = new Class0();
+            c01.nome = "c01";
+
+            Class1 c1 = new Class1();
+            c1.func();
+            c1.staticfunc();
+            Class1.staticfunc();
+
+            //una classe astratta non è istanziabile
+            Class2 c2 = new Class2();
+            c2.func();
+            c2.staticfunc();
+            Class2.staticfunc();
+
+            //una classe statica non è istanziabile
+            Class3 c3 = new Class3();
+            c3.func();
+            c3.staticfunc();
+            Class3.staticfunc();
+
+            Class4 c4 = new Class4();
+            c4.func();
+            c4.staticfunc();
+            Class4.staticfunc();
+
+            Class5 c5 = new Class5();
+            c5.func();
+            c5.staticfunc();
+            Class5.staticfunc();
+
+            Class6 c6 = new Class6();
+            c6.func();
+            c6.staticfunc();
+            Class6.staticfunc();
+
+            Class7 c7 = new Class7();
+            c7.func();
+            c7.staticfunc();
+            Class7.staticfunc();
+        }
+    }
+}
